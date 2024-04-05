13e65514d91e36773cdc8132413978a754706482
Kobbe <jakub.korban@microsoft.wsei.edu.pl>
2024-04-05 11:38:28 +0200

    note v0.1

--- Lab 4/main.js
+++ Lab 4/main.js
@@ -1,10 +1,24 @@
-// daty
-const terazTimestamp = Date.now()
-const teraz = new Date(terazTimestamp)
-console.log(teraz.toLocaleString())
+const addButton = document.querySelector('button[type="submit"]');
+const dateElement = document.querySelector('#date');
+
+// dates
+const nowTimestamp = Date.now()
+const now = new Date(nowTimestamp)
+console.log(now.toLocaleString())
 
 // localStorage
 // zapisywanie
 // localStorage.setItem(key, value)
 // pobieranie
 // localStorage.getItem(key)
+
+
+addButton.addEventListener('click', function() {
+    const formattedDate = now.getFullYear() 
+    + '-' + (now.getMonth() + 1).toString().padStart(2, '0') 
+    + '-' + now.getDate().toString().padStart(2, '0')
+    + '\u00A0\u00A0' + now.getHours().toString().padStart(2, '0')
+    + ':' + now.getMinutes().toString().padStart(2, '0')
+    + ':' + now.getSeconds().toString().padStart(2, '0');
+    dateElement.textContent = formattedDate;
+});

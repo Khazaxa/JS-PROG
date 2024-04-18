64fa3827b3a21b7a618745a4f9e216779305a7e0
Kobbe <jakub.korban@microsoft.wsei.edu.pl>
2024-04-18 11:08:32 +0200

    .

--- Lab 7/script.js
+++ Lab 7/script.js
@@ -1,12 +1,25 @@
-let counter = 0
-let lastTime = Date.now()
-window.addEventListener('deviceorientation', onDeviceMove)
-
-function onDeviceMove(event) {
-    console.log(event)
-}
-
 function animate() {
+    const elapsed = Date.now() - startTime;
+    const minutes = Math.floor(elapsed / 60000);
+    const seconds = Math.floor((elapsed % 60000) / 1000);
+    const hundredths = Math.floor((elapsed % 1000) / 10);
+    
+    const maxAngle = 45; 
+    const boardRect = board.getBoundingClientRect(); 
+
+    const x = (gamma / maxAngle) * (boardRect.width - ball.offsetWidth);
+    const y = (beta / maxAngle) * (boardRect.height - ball.offsetHeight);
+
+    // Obliczamy procenty x i y
+    const xPercent = (x / boardRect.width) * 100;
+    const yPercent = (y / boardRect.height) * 100;
+
+    // Ustawiamy left i top dla piłki na procenty x i y
+    ball.style.left = `${xPercent}%`;
+    ball.style.top = `${yPercent}%`;
+
+    document.querySelector('#time').textContent = `${minutes}:${seconds.toString().padStart(2, '0')}.${hundredths.toString().padStart(2, '0')}`;
+
     counter++
     if (counter % 100 === 0) {
         const time = Date.now()
